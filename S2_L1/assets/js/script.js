e20dcc74948e05ad3dbd5be1e103bc41f89c71ec
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-29 21:51:05 +0200

    Create script.js

---
+++ S2_L1/assets/js/script.js
@@ -0,0 +1,5 @@
+
+function popup() {
+    alert('Primo alert');
+    alert('Secondo Alert');
+}
---
+++ S2_L1/assets/js/script2.js
@@ -0,0 +1,15 @@
+
+document.write('<p>output scritto da Javascript<p>');
+document.write('<p>output scritto da Javascript<p>');
+
+var title = 'Iniziamo la nostra pratica';
+document.getElementById('title').innerHTML = title;
+
+var myNaming = 'criceto';
+console.log(myNaming);
+
+var MyNaming = 'giraffa';
+console.log(MyNaming);
+
+var my_Naming = 'leone';
+console.log(my_Naming);
