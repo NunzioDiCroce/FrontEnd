2b1fb2075bacf7dc54c2c97fe0ca2d85d1eed28d
NunzioDiCroce <nunziodavide@gmail.com>
2023-09-03 19:12:41 +0200

    Update script.js

---
+++ S3_L4/assets/js/script.js
@@ -0,0 +1,21 @@
+
+/* function for button click handling */
+const clickHandling = function() {
+
+    /* check if no task in input field*/
+    if(document.querySelector('#newTask input').value.length === 0) {
+        alert('Insert a new task!')
+    } else {
+        createTask();
+    }
+}
+
+const createTask = function() {
+    
+}
+
+/* addEventListener on button click at window.onload */
+window.onload = function() {
+    let createTaskBtn = document.querySelector('#createTaskBtn');
+    createTaskBtn.addEventListener('click', clickHandling)
+}
