7418a382e5781eb0b063ea9c5d7186c6bba68735
NunzioDiCroce <nunziodavide@gmail.com>
2023-09-04 08:54:54 +0200

    Complete createTask method

--- S3_L4/assets/js/script.js
+++ S3_L4/assets/js/script.js
@@ -6,14 +6,28 @@ const clickHandling = function() {
     if(document.querySelector('#newTask input').value.length === 0) {
         alert('Insert a new task!')
     } else {
+
+        /* method to create a new task */
         createTask();
     }
 }
 
 const createTask = function() {
 
+    let input = document.querySelector('#newTask input');
     let tasks = document.querySelector('#tasks');
+
+    /* newTask item definition */
+    let newTask =
+        `<div class="task">
+            <span id="taskname">${input.value}</span><button class="delete"><i class="far fa-trash-alt"></i></button>
+        </div>`
     
+    /* newTsk item position into DOM */
+    tasks.innerHTML += newTask;
+
+    /* input.value at blank after click */
+    input.value = '';
 }
 
 /* addEventListener on button click at window.onload */
